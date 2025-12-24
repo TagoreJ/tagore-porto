ace0ab3d6ff7b19a45099d288e615875fdab31ce
tagorej.ssshil <tagorej.ssshil@gmail.com>
2025-12-24 06:10:48 +0000

    Fix: Correct Vercel Analytics import path

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics";
 
 function App() {
   return (
